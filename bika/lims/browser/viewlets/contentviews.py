8091dc4ad59cc69c92126c79c4f4104c46ee5bac
Ramon Bartl <rb@ridingbytes.com>
2019-10-29 12:33:02 +0100

    Content Views Viewlet added

---
+++ bika/lims/browser/viewlets/contentviews.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+
+from plone.app.layout.viewlets.common import ContentViewsViewlet as Base
+from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
+
+
+class ContentViewsViewlet(Base):
+    index = ViewPageTemplateFile("templates/contentviews.pt")
